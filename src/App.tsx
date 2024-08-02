f34fdcf3de81c641cacef5d24333c120f4f65693
Daoxi Sun <daoxisun@gmail.com>
2024-08-02 03:57:31 -0400

    added the basic functionalities for submitting to the list from user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,44 @@
-export default function App () {
-	return "The start of my new project"
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+	const [newItem, setNewItem] = useState("");
+	const [todos, setTodos] = useState<any[]>([]); //useState([]) infers the type never[] from the default value [], so don't use it in TypeScript
+
+	// the argument is an event object, just leaving it as any type for now
+	function handleSubmit(e: any) {
+		e.preventDefault(); //prevent page from refreshing
+
+		//updating the state
+		setTodos((currentTodos) => {
+			return [...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false }];
+		});
+
+		setNewItem(""); //clears the field everytime it's submitted
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} className="new-item-form">
+				<div className="form-row">
+					<label htmlFor="item">New Item</label>
+					<input /* Set the value to use the state hook and update it using onChange */ value={newItem} onChange={(e) => setNewItem(e.target.value)} type="text" id="item"></input>
+				</div>
+				<button className="btn">Add</button>
+			</form>
+			<h1 className="header">My list</h1>
+			<ul className="list">
+				{todos.map((todo) => {
+					return (
+						<li key={todo.id}>
+							<label>
+								<input type="checkbox" checked={todo.completed} />
+								{todo.title}
+							</label>
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
 }
