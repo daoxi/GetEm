54763392577c45b26a77a21a0e257fd4096c2a72
Daoxi Sun <daoxisun@gmail.com>
2024-08-10 00:38:31 -0400

    configured Vite for build and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	base: '', //setting base to an empty string, making the path relative to its deployment directory
 })
