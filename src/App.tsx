cfe13de6e64a8916341e1663633022fdf182d418
Daoxi Sun <daoxisun@gmail.com>
2024-08-11 14:04:56 -0400

    restructured code files into the folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,10 @@
-import { useState, useEffect } from "react";
-import { NewItemForm } from "./NewItemForm";
-import { ItemList } from "./ItemList";
-import "./styles.scss";
+import Checklist from "./Checklist";
 
-export default function App() {
-	//Use localStorage for initializing state unless if it's empty
-	const [items, setItems] = useState<any[]>(() => {
-		const myItems = localStorage.getItem("myItems");
-		if (myItems === null) {
-			return [];
-		} else {
-			return JSON.parse(myItems);
-		}
-	});
-
-	//Save the data in localStorage whenever the state changes
-	useEffect(() => {
-		localStorage.setItem("myItems", JSON.stringify(items));
-	}, [items]);
-
-	//Adding item to the current state array, this function will also be used in other component(s)
-	function addItem(title: string) {
-		setItems((currentItems) => {
-			return [...currentItems, { id: crypto.randomUUID(), title: title, completed: false }];
-		});
-	}
-
-	//Toggling the checkbox on/off for the item
-	function toggleItem(id: number, completed: boolean) {
-		setItems((currentItems: any) => {
-			return currentItems.map((item: any) => {
-				//check each item and update the one that fired the onChange event
-				if (item.id === id) {
-					return { ...item, completed: completed };
-				}
-				return item;
-			});
-		});
-	}
-
-	//Editing text content of an existing item
-	function editItem(id: number, title: string) {
-		setItems((currentItems: any) => {
-			return currentItems.map((item: any) => {
-				//check each item and update the one that fired the onChange event
-				if (item.id === id) {
-					return { ...item, title: title };
-				}
-				return item;
-			});
-		});
-	}
-
-	//Removing item for which the user clicked the delete button
-	function deleteItem(id: number) {
-		setItems((currentItems: any) => {
-			//using filter() instead of map() as the element to be deleted can't be properly mapped
-			return currentItems.filter((item: any) => item.id !== id);
-		});
-	}
-
-	return (
-		<>
-			<NewItemForm addItem={addItem} /> {/* passing the function as props */}
-			<ItemList items={items} toggleItem={toggleItem} editItem={editItem} deleteItem={deleteItem} />
-		</>
-	);
+function App () {
+	return (<>
+	<h1>Working</h1>
+	<Checklist />
+	</>)
 }
+
+export default App
