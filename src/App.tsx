2c9ecfa928b416366f96b0757b4dcd4d0d2ff216
Daoxi Sun <daoxisun@gmail.com>
2024-08-14 01:11:33 -0400

    imported Bootstrap and router libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+//Bootstrap contains default styling for most HTML elements, which will be applied to all components
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
 import Checklist from "./Checklist";
 
-function App () {
-	return (<>
-	<h1>Working</h1>
-	<Checklist />
-	</>)
+function App() {
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<>
+						<h1>Working</h1>
+						<Checklist />
+					</>
+				}
+			></Route>
+		</Routes>
+	);
 }
 
-export default App
+export default App;
