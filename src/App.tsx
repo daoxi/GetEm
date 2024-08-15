9ddce8a59171b16b67495482d7e24d4df2ea4129
Daoxi Sun <daoxisun@gmail.com>
2024-08-14 22:15:34 -0400

    created routes for static and dynamic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-//Bootstrap contains default styling for most HTML elements, which will be applied to all components
+//Bootstrap allows styling using className, also contains default styling for most elements, which will be applied to all components
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Checklist from "./Checklist";
+import { Container } from "react-bootstrap";
 
 function App() {
 	return (
+		<Container className="my-4">
 		<Routes>
 			<Route
 				path="/"
@@ -14,8 +16,23 @@ function App() {
 						<Checklist />
 					</>
 				}
-			></Route>
+			/>
+			<Route
+				path="/new"
+				element={
+					<>
+						<h1>New</h1>
+					</>
+				}
+			/>
+			<Route path="/:id">
+				<Route index element={<h1>Show</h1>}/>
+				<Route path="edit" element={<h1>Edit</h1>} />
+			</Route>
+			{/* fallback route that goes back to homepage for unrecognized route */}
+			<Route path="*" element={<Navigate to="/"/>} />
 		</Routes>
+		</Container>
 	);
 }
 
