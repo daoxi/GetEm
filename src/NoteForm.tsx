92d6fce149341b5c4519d03758ba5c3ec5394ee8
Daoxi Sun <daoxisun@gmail.com>
2024-08-23 18:56:13 -0400

    added reference and submit function

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,18 +1,30 @@
 //Used for both creating new and and editing existing notes
 
+import { FormEvent, useRef } from "react";
 import { Form, Stack, Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 
 export function NoteForm() {
+	const titleRef= useRef<HTMLInputElement>(null);
+	const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+function handleSubmit(e: FormEvent){
+	e.preventDefault();
+
+
+	
+}
+
 	return (
 		<>
-			<Form>
+			<Form onSubmit={handleSubmit}>
 				<Stack gap={4}>
 					<Row>
 						<Col>
 							<Form.Group controlId="title">
 								<Form.Label>Title</Form.Label>
-								<Form.Control required />
+								<Form.Control ref={titleRef} required />
 							</Form.Group>
 						</Col>
 						<Col>
@@ -24,15 +36,17 @@ export function NoteForm() {
 					</Row>
 					<Form.Group controlId="markdown">
 						<Form.Label>Body</Form.Label>
-						<Form.Control required as="textarea" rows={18} />
+						<Form.Control required as="textarea" ref={markdownRef} rows={18} />
 					</Form.Group>
 					<Stack direction="horizontal" gap={3} className="justify-content-end">
 						<Button type="submit" variant="primary">
 							Save
 						</Button>
-						<Button type="button" variant="outline-secondary">
-							Cancel
-						</Button>
+						<Link to=".." /* go back to the previous visited page */ >
+							<Button type="button" variant="outline-secondary">
+								Cancel
+							</Button>
+						</Link>
 					</Stack>
 				</Stack>
 			</Form>
