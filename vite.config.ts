75b990d2bea085361db326a89933db2be5d24e20
Daoxi Sun <daoxisun@gmail.com>
2024-10-21 01:18:57 -0400

    removed base and homepage config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-	base: '', //setting base to an empty string, making the path relative to its deployment directory
-})
+	plugins: [react()],
+	//base: '', //setting base (which is '/' by default) to an empty string (equivalent to setting to './'), making the path relative to its deployment directory
+});
